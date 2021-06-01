5db9d424cf4247e8fe96432c665462d0d58c1c46
Anuj0103 <Anuj@0103>
2021-06-01 23:38:12 +0530

    fixing bugs

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
+const express=require('express');
+const bcrypt=require('bcrypt-nodejs');
+const knex = require('knex')
 const cors = require('cors');
-const knex = require('knex');
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-//hello
-
-const db = knex({
+const register=require('./controllers/register')
+const signin=require('./controllers/signin')
+const profile=require('./controllers/profile')
+const image=require('./controllers/image')
+const db=knex({
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl:  true,
-  },
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'admin',
+    database : 'smart-brain'
+  }
 });
 
-const app = express();
 
-app.use(cors());
+
+const app=express()
+
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('this is working bro!');
-});
-
-app.post('/signin', (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt);
-});
-
-app.post('/register', (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
-});
-
-app.get('/profile/:id', (req, res) => {
-  profile.handleProfile(req, res, db);
-});
-
-app.put('/image', (req, res) => {
-  image.handleImage(req, res, db);
-});
-
-app.post('/imageUrl', (req, res) => {
-  image.handleApiCall(req, res);
-});
+app.use(cors())
 
 
-const PORT = process.env.PORT;
 
-app.listen(PORT||3050, () => {
-  console.log(`app is running on port ${PORT}`);
+app.get('/',(req,res)=>{
+	res.send(db.select('*').from('users'));
 })
+
+app.post('/signin',(req,res)=>{ signin.handlesignin(req,res,db,bcrypt)})
+
+app.post('/register',(req,res)=>{ register.handleregister(req,res,db,bcrypt)})
+
+app.get('/profile/:id',(req,res)=>{ profile.handleprofile(req,res,db)})
+
+app.put('/image',(req,res)=>{ image.handleimage(req,res,db)})
+
+app.post('/imageurl',(req,res)=>{ image.handleapi(req,res)})
+
+
+
+// Load hash from your password DB.
+
+
+
+app.listen(3000,()=>{
+	console.log('app is running on port 3000');
+});
+
 /*
 /-->res=this is working
 /signin-->POST=success/fail
