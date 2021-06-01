c41e6c157b95a1adfbddad579d577a9e66e9b8e3
Anuj0103 <Anuj@0103>
2021-06-01 13:20:11 +0530

    fix

--- server.js
+++ server.js
@@ -8,14 +8,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 //hello
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'admin',
-    database: 'smart-brain',
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   },
 });
 
